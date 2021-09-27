7963bbda4c3b3305cd69a165d96728044d595c0e
PDG <PDG@192.168.219.104>
2021-09-27 23:54:49 +0900

    댓글처리중

---
+++ ex3/src/test/resources/sql/sql.sql
@@ -0,0 +1,38 @@
+
+create SEQUENCE seq_board;
+
+create table tbl_board(
+    bno number(10,0),
+    title varchar2(200) not null,
+    content varchar2(2000) not null,
+    writer varchar2(50) not null,
+    regdate date default sysdate,
+    updatedate date default sysdate
+);
+
+alter table tbl_board add CONSTRAINT pk_board
+primary key (bno);
+
+
+insert into tbl_board(bno, title, content, writer)
+values (SEQ_BOARD.nextval, '테스트 제목', '테스트 내용', 'user00');
+
+
+select * from tbl_board where bno =5; 
+
+
+-- 댓글 추가하기 위한 테이블 생성
+create table tbl_reply(
+    rno number(10,0),
+    bno number(10,0) not null,
+    reply varchar2(1000) not null,
+    replyer varchar2(50) not null,
+    replyDate date default sysdate,
+    updateDate date default sysdate
+);
+
+create SEQUENCE seq_reply;
+
+alter table tbl_reply add constraint pk_reply primary key (rno);
+alter table tbl_reply add constraint fk_reply_board foreign key (bno) references tbl_board (bno);
+
